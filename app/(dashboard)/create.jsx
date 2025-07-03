4b9fa13e5df8703a8c5b2dddda854a91a746abd8
Liam Jackson <liamjacksonbusiness@gmail.com>
2025-07-03 15:03:27 +1000

    Create book page + functionality
    Added the create book page UI + functionality

--- app/(dashboard)/create.jsx
+++ app/(dashboard)/create.jsx
@@ -1,16 +1,78 @@
-import { StyleSheet } from "react-native";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import { StyleSheet, Text } from "react-native";
+import { useBooks } from "../../hooks/useBooks";
+
 import Spacer from "../../components/Spacer";
+import ThemedButton from "../../components/ThemedButton";
 import ThemedText from "../../components/ThemedText";
+import ThemedTextInput from "../../components/ThemedTextInput";
 import ThemedView from "../../components/ThemedView";
 
 const CreateScreen = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [description, setDescription] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const { createBook } = useBooks();
+  const router = useRouter();
+
+  const handleSubmit = async () => {
+    if (!title.trim() || !author.trim() || !description.trim()) return;
+
+    setLoading(true);
+
+    await createBook({ title, author, description });
+
+    // reset form fields
+    setTitle("");
+    setAuthor("");
+    setDescription("");
+
+    // redirect
+    router.replace("/books");
+
+    // reset loading state
+    setLoading(false);
+  };
+
   return (
-    <ThemedView style={styles.container} safe>
+    // <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+    <ThemedView style={styles.container}>
       <ThemedText title style={styles.heading}>
-        Create a New Book
+        Add a new Book
       </ThemedText>
       <Spacer />
+      <ThemedTextInput
+        style={styles.input}
+        placeholder="Book Title"
+        value={title}
+        onChangeText={setTitle}
+      />
+      <Spacer />
+      <ThemedTextInput
+        style={styles.input}
+        placeholder="Author"
+        value={author}
+        onChangeText={setAuthor}
+      />
+      <Spacer />
+      <ThemedTextInput
+        style={styles.multiline}
+        placeholder="Book Description"
+        value={description}
+        onChangeText={setDescription}
+        multiline={true}
+      />
+      <Spacer />
+      <ThemedButton onPress={handleSubmit} disabled={loading}>
+        <Text style={{ color: "#fff" }}>
+          {loading ? "Saving..." : "Create Book"}
+        </Text>
+      </ThemedButton>
     </ThemedView>
+    // </TouchableWithoutFeedback> Test this on actual phone
   );
 };
 
@@ -26,5 +88,18 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "bold",
     textAlign: "center",
+  },
+  input: {
+    padding: 20,
+    borderRadius: 6,
+    alignSelf: "stretch",
+    marginHorizontal: 40,
+  },
+  multiline: {
+    padding: 20,
+    borderRadius: 6,
+    minHeight: 100,
+    alignSelf: "stretch",
+    marginHorizontal: 40,
   },
 });
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,6 +2,7 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { useColorScheme } from "react-native";
 import { Colors } from "../constants/Colors";
+import { BooksProvider } from "../contexts/BooksContext";
 import { UserProvider } from "../contexts/UserContext";
 
 const RootLayout = () => {
@@ -10,18 +11,20 @@ const RootLayout = () => {
 
   return (
     <UserProvider>
-      <StatusBar value="auto" />
-      <Stack
-        screenOptions={{
-          headerStyle: { backgroundColor: theme.navBackground },
-          headerTintColor: theme.title,
-          headerTitleAlign: "center",
-        }}
-      >
-        <Stack.Screen name="index" options={{ title: "Home" }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
-      </Stack>
+      <BooksProvider>
+        <StatusBar value="auto" />
+        <Stack
+          screenOptions={{
+            headerStyle: { backgroundColor: theme.navBackground },
+            headerTintColor: theme.title,
+            headerTitleAlign: "center",
+          }}
+        >
+          <Stack.Screen name="index" options={{ title: "Home" }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
+        </Stack>
+      </BooksProvider>
     </UserProvider>
   );
 };
